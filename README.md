5da9d1cb762abdc8b5a94531efd1b2bd9c7500e1
excalibur-kvrv <excalibur.krv@gmail.com>
2020-02-08 17:02:29 +0530

    refactored code and added installation instructions

--- README.md
+++ README.md
@@ -1,18 +1,29 @@
 # m3u8-playlist-downloader
-a script to download videos in a m3u8 playlis, write it to a single video file and convert it to mp4 using ffmpeg 
+a CLI program to download videos in a m3u8 playlist, write it to a single video file and convert it to mp4 using ffmpeg 
 
-# dependencies
-- requests module install using `pip install requests`<br>
-- ffmpeg for video conversion visit https://www.ffmpeg.org/download.html
+## Dependencies
+- Requests module install using `pip install requests`<br>
+- FFMPEG for video conversion visit https://www.ffmpeg.org/download.html<br>
+- PyInstaller for building executable `pip install pyinstaller`<br>
 
-# usage
+## Usage without installation
 - create a virtual environment using `virtualenv -p python3.6 venv` in linux<br/>
 - activate virtual environment using `source venv/bin/activate` in linux<br/>
 - insert the url request headers in headers.txt<br/>
 - start the script using `python m3u8_playlist_downloader.py <url of playlist>`
 
-# options
+## Installing/Uninstalling
+for installation in ubuntu using PyInstaller:-
+- active virtualenv using `source venv/bin/activate`
+- run `pyinstaller m3u8_playlist_downloader.py --name m3u8-dl --onefile --hidden-import=requests`
+- run `sudo mv dist/m3u8-dl /usr/local/bin` in terminal window
 
+for uninstall in ubuntu:-
+- run `sudo rm /usr/local/bin/m3u8-dl`
+
+## CLI Options
 - `--help, -h:- display how to use the script`<br>
 - `--convert, -c:- specify this flag to convert the video to mp4 using ffmpeg`<br>
 - `--name, -n:- specify the name by which to save the downloaded video, ele 'video' is chosen as default name`<br>
+- `--header-path, -p:- specify the path of header file`<br>
+- `--force, -f:- specify to re-download the video if it exists`<br>
--- m3u8_playlist_downloader.py
+++ m3u8_playlist_downloader.py
@@ -1,11 +1,10 @@
 from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor
-from multiprocessing import cpu_count, current_process
+from multiprocessing import cpu_count, current_process, Lock
 from urllib.parse import urlparse, urljoin
 from traceback import print_exc
 from time import sleep, time
 from pprint import pprint
 from random import randint
-import multiprocessing as mp
 import subprocess
 import argparse
 import requests
@@ -16,16 +15,23 @@ import os
 header = {}
 logging.basicConfig(filename='app.log', filemode='w',
                     format='%(name)s - %(levelname)s - %(message)s', level=logging.ERROR)
-mpLock = mp.Lock()
+mpLock = Lock()
 total_content = 0
 THREAD_NUM = cpu_count()
+TEMP_FOLDER = ".asdalkshdalskdhalsdhaslk12313123asdllcvsd"
 
 
-def construct_headers():
+def directory_validator(string):
+    if os.path.isfile(string):
+        return string
+    raise FileNotFoundError(f"{path} does not point to a file")
+
+
+def construct_headers(header_path: str):
     global header
     print("Request headers\n")
-    if os.path.exists("headers.txt"):
-        with open("headers.txt") as file:
+    if os.path.exists(header_path):
+        with open(header_path) as file:
             lines = file.readlines()
             lines = [line.strip() for line in lines]
         for line in lines:
@@ -41,15 +47,17 @@ def construct_headers():
                 "headers incorrectly parsed check construct_headers() function for debugging")
             sys.exit()
     else:
-        print("Include headers in a headers.txt file and restart program")
+        print(f"Include headers in {header_path} directory and restart program")
         sys.exit()
 
 
 def convert_video(video_input, video_output):
     # These arguments will be passed in with ffmpeg for video conversion
     flags = ["ffmpeg", "-i", video_input, "-f", "mp4", "-vcodec", "libx264", "-preset",
-            "ultrafast", "-profile:v", "main", "-acodec", "aac", video_output, "-hide_banner"]
-    subprocess.Popen(flags)
+             "ultrafast", "-profile:v", "main", "-acodec", "aac", video_output, "-hide_banner"]
+    process = subprocess.Popen(flags)
+    process.wait()
+    os.unlink(video_input)
 
 
 def fetch_data(base_url: str, file_name: str, header: dict):
@@ -59,19 +67,20 @@ def fetch_data(base_url: str, file_name: str, header: dict):
         try:
             sleep(randint(10, 30))
             req = requests.get(base_url, headers=header, timeout=60)
-        except:
+        except Exception as err:
             with mpLock:
+                logging.error(err)
                 with open("error_links.txt", "a") as file:
                     file.write(f"{base_url}\n")
             return
-
-    with open(file_name, "wb") as file:
+    temp_path = os.path.join(TEMP_FOLDER, file_name)
+    with open(temp_path, "wb") as file:
         file.write(req.content)
 
 
 def download_thread(i: int, link: str):
     file_name = f"{i}"
-    if os.path.exists(file_name):
+    if os.path.exists(os.path.join(TEMP_FOLDER, file_name)):
         return
     fetch_data(link, file_name, header)
 
@@ -92,6 +101,9 @@ def initialize_threads(links: list, start: int):
 
 def initialize_parallel_threads(links: list):
     # No: of processes started depend on the number of cores available
+
+    os.makedirs(TEMP_FOLDER, exist_ok=True)
+
     process_number: int = cpu_count()
     print(f"initializing {process_number} processes")
     with ProcessPoolExecutor(max_workers=process_number) as processes:
@@ -114,15 +126,15 @@ def write_file(name: str):
 
     with open(name, "ab") as movie_file:
         files = []
-        for file in os.listdir():
+        for file in os.listdir(TEMP_FOLDER):
             if file.isnumeric():
                 files.append(file)
         for file in sorted(files, key=int):
-            print(f"writing file {file}")
-            with open(file, "rb") as movie_chunk:
+            with open(os.path.join(TEMP_FOLDER, file), "rb") as movie_chunk:
                 movie_file.write(movie_chunk.read().strip())
-            os.unlink(file)
+            os.unlink(os.path.join(TEMP_FOLDER, file))
     os.unlink("links.txt")
+    os.rmdir(TEMP_FOLDER)
 
 
 if __name__ == "__main__":
@@ -130,19 +142,26 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("url", help="Pass in a url containing m3u8 playlist", type=str)
     parser.add_argument("-n", "--name", type=str, help="Specify a name to save the downloaded video as, if no name is "
-                                                       "specified default name of 'video' will be chosen") 
+                                                       "specified default name of 'video' will be chosen")
+    parser.add_argument("-p", "--header-path", type=directory_validator,
+                        help="Specify the path to the file containing headers, if no path is specified the program "
+                             "will look for a headers.txt file in the same directory")
+    parser.add_argument("-f", "--force", action="store_true", help="If this flag is used and the video has been "
+                                                                   "downloaded the download will restart")
     parser.add_argument("-c", "--convert", help="Convert the downloaded video to mp4 using ffmpeg", action="store_true")
+
     args = parser.parse_args()
 
     print("Logs for the download will be stored in app.log")
 
-    if "url" in args:
-        url = args.url
-    else:
-        url = input("Enter a url containing m3u8 playlist\n")
+    url = args.url
 
-    # This function is responsible for parsing the headers provided in headers.txt
-    construct_headers()
+    if args.header_path:
+        path = args.header_path
+    else:
+        path = "headers.txt"
+    # This function is responsible for parsing the headers file provided in path
+    construct_headers(path)
 
     start_time = time()
 
@@ -157,7 +176,7 @@ if __name__ == "__main__":
         temp = file.readlines()
         temp = [link.strip() for link in temp]
 
-    parsed_url: str = urlparse(url)
+    parsed_url = urlparse(url)
     base_url: str = f"{parsed_url.scheme}://{parsed_url.netloc}{'/'.join(parsed_url.path.split('/')[:-1])}/"
 
     # construct a links array list contains the links joined with the base_url.
@@ -165,22 +184,27 @@ if __name__ == "__main__":
                    for link in temp if "EXT" not in link]
 
     total_content += len(links)
-    try:
-        # This function is responsible initializing the processes that will make the requests to the links.
-        initialize_parallel_threads(links)
 
-        if "name" in args:
+    try:
+        if args.name:
             name = args.name
         else:
             name = "video"
-        # This links is responsible for writing video file
-        write_file(name)
+
+        # if the video has already been downloaded the below, re-download will be skipped unless --force is used
+        if not os.path.exists(name) or args.force:
+            initialize_parallel_threads(links)
+            write_file(name)
+
+        elapsed_time = time() - start_time
+        print(f"Download took {elapsed_time} seconds")
 
         # Begin video conversion if -c or --convert argument is passed in.
         if args.convert:
+            print("Beginning conversion to mp4")
             convert_video(name, f"{name}.mp4")
+            print("Completed conversion to mp4")
 
-        elapsed_time = time() - start_time
-        print(f"Elapsed time is {elapsed_time} seconds")
     except Exception as err:
+        logging.error(err)
         print_exc()
